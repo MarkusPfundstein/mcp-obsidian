781748652cce5559e367061b78fa3e759c1a9b49
Tony <mobilestack@users.noreply.github.com>
2025-04-11 15:38:56 +0800

    added base url for docker or remote server

--- src/mcp_obsidian/obsidian.py
+++ src/mcp_obsidian/obsidian.py
@@ -1,6 +1,12 @@
+import os
+from dotenv import load_dotenv
 import requests
 import urllib.parse
 from typing import Any
+
+load_dotenv()
+
+base_url = os.getenv("BASE_URL", "")
 
 class Obsidian():
     def __init__(
@@ -19,7 +25,10 @@ class Obsidian():
         self.timeout = (3, 6)
 
     def get_base_url(self) -> str:
-        return f'{self.protocol}://{self.host}:{self.port}'
+        if base_url != "":
+            return base_url
+        else:
+            return f'{self.protocol}://{self.host}:{self.port}'
     
     def _get_headers(self) -> dict:
         headers = {
